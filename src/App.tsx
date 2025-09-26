22e10faf1934613ddc5d43a5309f8ee74d922ebd
Donghyun Lee <ldhhello@naver.com>
2025-09-27 00:53:14 +0900

    ✨ Add route 'me'

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Index from './routes'
+import Me from './routes/me'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Index />} />
+        <Route path='/me' element={<Me />} />
         <Route path='*' element={<div>Not found</div>} />
       </Routes>
     </BrowserRouter>
--- src/routes/me.tsx
+++ src/routes/me.tsx
@@ -1,9 +1,21 @@
 import ProjectItem from '../components/ProjectItem';
 import dimiMonsterImage from '../assets/DIMIMONSTER.png';
 import CompetitionItem from '../components/CompetitionItem';
+import { styled } from 'styled-components';
 
-export default function Index() {
-  return <>
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  ul {
+    padding: 0;
+    margin-top: 3rem;
+  }
+`;
+
+export default function Me() {
+  return <Container>
     <h1>Projects</h1>
     <ProjectItem
       title="DIMIMONSTER"
@@ -22,5 +34,5 @@ export default function Index() {
         세종정보올림피아드 시범대회 <strong>금상</strong> (2019)
       </CompetitionItem>
     </ul>
-  </>
+  </Container>
 }
