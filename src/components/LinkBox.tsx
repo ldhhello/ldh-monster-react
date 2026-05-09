aa2fe6af6bd6e367e5e0d45a0b9c7783db1f7e75
Donghyun Lee <ldhhello@naver.com>
2026-05-09 14:47:35 +0900

    ✨ BOJ 섭종에 따른 BOJ -> solved.ac 링크로 변경

--- src/components/LinkBox.tsx
+++ src/components/LinkBox.tsx
@@ -19,7 +19,7 @@ export default function LinkBox() {
   return <Container>
     <a href="https://www.instagram.com/ldhhello"><img src={instagramIcon}/></a>
     <a href="https://www.github.com/ldhhello"><img src={githubIcon}/></a>
-    <a href="https://www.acmicpc.net/user/ldhhello"><img src={codeSlashIcon}/></a>
+    <a href="https://solved.ac/profile/ldhhello"><img src={codeSlashIcon}/></a>
     <a href="https://blog.naver.com/ldhhello"><img src={chatSquareDotsIcon}/></a>
   </Container>
 }
